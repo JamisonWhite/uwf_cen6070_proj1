28171cbc51a2f4c764a1565c47320e8d1b373c74
Jamie White <jwhite@appriver.com>
2015-09-29 09:42:48 -0500

    Cleaned up test cases and samples. Added comments for TODO tests

--- BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
+++ BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
@@ -1,5 +1,14 @@
+/*
+ * UWF CEN6070 Fall 2015 Project 1
+ *   Justin Lambert
+ *   Salina Hall
+ *   Jamie White
+ *   Michael Worn
+ */
 package basictokenizer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
@@ -14,218 +23,105 @@ import java.util.concurrent.Callable;
  */
 public class BasicTokenizerTester {
 
-   //=============================================
-   // Static class attributes
-   //=============================================
 
-    /**
-     * The total number of tests executed
-     */
-    public static int testingCount;
+    //=============================================
+    // Public Test Case Methods
+    //=============================================
+    private static void executeTestCases(List<TestCase> testCases, List<TestCase> exceptionCases) {
 
-    /**
-     * The total number of passing tests
-     */
-    public static int passingCount;
+        int testCaseCount = 0;
+        int passingCaseCount = 0;
+        int failedCaseCount = 0;
 
-    /**
-     * The total number of failed tests
-     */
-    public static int failureCount;
-    
-   //=============================================
-   // Public Test Case Methods
-   //=============================================
-
-    /**
-     * Test case T001
-     * Test of constructor BasicTokenizer(str)
-     *
-     * @return true test passes, false otherwise
-     */
-    public static boolean testCase001() {
-
-        //Arrange
-        BasicTokenizer tokenizer;
-        
-        //Act
-        tokenizer = new BasicTokenizer(null);
-        
-        //Assert
-        return tokenizer != null;
-        
-        
-//        boolean testingPassed = true;
-//        
-//        // T001 test of constructor BasicTokenizer(str)
-//        try {
-//             
-//            testingPassed = false;
-//        } catch (java.lang.NullPointerException e) {
-//            // expected result
-//        } catch (Exception e) {
-//            testingPassed = false;
-//        }
-//        
-//        return testingPassed;
-    }
-
-    /**
-     * Test case T002
-     * Test of constructor BasicTokenizer(str)
-     *
-     * @return true test passes, false otherwise
-     */
-    public static boolean testCase002() {
-        boolean testingPassed = true;
-        
-        // T001 test of constructor BasicTokenizer(str)
-        try {
-            BasicTokenizer tokenizer = new BasicTokenizer(null);
-             
-            testingPassed = false;
-        } catch (java.lang.NullPointerException e) {
-            // expected result
-        } catch (Exception e) {
-            testingPassed = false;
-        }
-        
-        return testingPassed;
-    }
-
-    /**
-     * Test case T003
-     * Test of constructor BasicTokenizer(str)
-     *
-     * @return true test passes, false otherwise
-     */
-    public static boolean testCase003() {
-        boolean testingPassed = true;
-        
-        // T001 test of constructor BasicTokenizer(str)
-        try {
-            BasicTokenizer tokenizer = new BasicTokenizer(null);
-             
-            testingPassed = false;
-        } catch (java.lang.NullPointerException e) {
-            // expected result
-        } catch (Exception e) {
-            testingPassed = false;
-        }
-        
-        return testingPassed;
-    }
-
-    /**
-     * Test case T004
-     * Test of constructor BasicTokenizer(str)
-     *
-     * @return true test passes, false otherwise
-     */
-    public static boolean testCase004() {
-        boolean testingPassed = true;
-        
-        // T001 test of constructor BasicTokenizer(str)
-        try {
-            BasicTokenizer tokenizer = new BasicTokenizer(null);
-             
-            testingPassed = false;
-        } catch (java.lang.NullPointerException e) {
-            // expected result
-        } catch (Exception e) {
-            testingPassed = false;
-        }
-        
-        return testingPassed;
-    }
-    
-    public static boolean testIsItOne(int one) throws Exception {
-        return one == 1;
-    }
-
-    /**
-     * Throw unexpected exception while testing scenario
-     * @return
-     * @throws Exception 
-     */
-    public static boolean testThrowAnException() throws Exception {
-        throw new Exception("Test exception thrown.");
-    }
-
-    /**
-     * Execute test cases and report results.
-     *
-     * @param testCases
-     */
-    private static void executeTestCases(TreeMap<String, Callable<Boolean>> testCases) {
         //Call each of the tests and report results.
         System.out.println("---------------------------------");
         System.out.println("-- Starting Test Suite");
         System.out.println("---------------------------------");
-        
-        for (Map.Entry<String, Callable<Boolean>> testEntry : testCases.entrySet()) {
-            String testName = testEntry.getKey();
-            Boolean testResult = false;
-            String errorMessage = "";
 
+        for (TestCase testCase : testCases) {
+            testCaseCount++;
             try {
-                BasicTokenizerTester.testingCount++;
-                //System.out.println("[" + BasicTokenizerTester.testingCount + "] TESTING: " + testName);
-                testResult = testEntry.getValue().call();
+                if (testCase.execute()) {
+                    passingCaseCount++;
+                    System.out.println("[" + testCaseCount + "] PASSED: " + testCase.getName());
+                } else {
+                    failedCaseCount++;
+                    System.out.println("[" + testCaseCount + "] FAILED: " + testCase.getName());
+                }
             } catch (Exception ex) {
-                errorMessage = ex.getMessage();
-            }
-            
-            if (testResult) {
-                BasicTokenizerTester.passingCount++;
-                System.out.println("[" + BasicTokenizerTester.testingCount + "] PASSED: " + testName);
-            } else {
-                BasicTokenizerTester.failureCount++;
-                System.out.println("[" + BasicTokenizerTester.testingCount + "] FAILED: " + testName
-                        + (("".equals(errorMessage) ? "" : " ERROR: " + errorMessage)));
+                failedCaseCount++;
+                System.out.println("[" + testCaseCount + "] FAILED: " + testCase.getName() + " ERROR: " + ex.getMessage());
             }
         }
-        
+
+        for (TestCase testCase : exceptionCases) {
+            testCaseCount++;
+            try {
+                testCase.execute();
+                failedCaseCount++;
+                System.out.println("[" + testCaseCount + "] FAILED: " + testCase.getName());
+            } catch (Exception ex) {
+                passingCaseCount++;
+                System.out.println("[" + testCaseCount + "] PASSED: " + testCase.getName() + " ERROR: " + ex.getMessage());
+            }
+        }
+
         System.out.println("---------------------------------");
         System.out.println("-- Completed Test Suite");
-        System.out.println("-- " + BasicTokenizerTester.testingCount + " Total Tests");
-        System.out.println("-- " + BasicTokenizerTester.passingCount + " Passing Tests");
-        System.out.println("-- " + BasicTokenizerTester.failureCount + " Failing Tests");
+        System.out.println("-- " + testCaseCount + " Total Tests");
+        System.out.println("-- " + passingCaseCount + " Passing Tests");
+        System.out.println("-- " + failedCaseCount + " Failing Tests");
         System.out.println("---------------------------------");
+
     }
 
-   //=============================================
-   // Main method to start console app
-   //=============================================
-
+    //=============================================
+    // Main method to start console app
+    //=============================================
     /**
      * The test driver for class BasicTokenizer
      *
      * @param args
      */
     public static void main(String[] args) {
-      
+
+        List<TestCase> testCases = new ArrayList<TestCase>();
+        List<TestCase> errCases = new ArrayList<TestCase>();
+
+        //TODO We need the Decision table to help us narrow down the possibilities!
         
-        //Ordered HashMap with name of the test and a method call
-        TreeMap<String, Callable<Boolean>> testCases = new TreeMap<>();
+        //Test Constructors
+        
+        testCases.add(new TestConstructor1("TC001 -- Constructor 1 valid text.", "a b c d"));
+        testCases.add(new TestConstructor1("TC002 -- Constructor 1 empty text.", ""));
+        errCases.add(new TestConstructor1("TC003 -- Constructor 1 null text. Throws exception.", null));
+        testCases.add(new TestConstructor2("TC004 -- Constructor 2 valid text.", "a b c d", " "));
+        testCases.add(new TestConstructor2("TC005 -- Constructor 3 empty text.", "", ""));
+        errCases.add(new TestConstructor2("TC006 -- Constructor 4 null text. Throws exception.", null, null));
+        //etc....
 
-        testCases.put("Test Case T001 - Boundary Value test first constructor ",
-                () -> TestCase001.execute());
-        testCases.put("Test Case T002 - Boundary Value test first constructor ",
-                () -> BasicTokenizerTester.testCase002());
-        testCases.put("Test Case T003 - Boundary Value test first constructor ",
-                () -> BasicTokenizerTester.testCase003());
-        testCases.put("Test Case T004 - Boundary Value test first constructor ",
-                () -> BasicTokenizerTester.testCase004());
-        testCases.put("IsItOne1 - demo sending parameters to test method. ",
-                () -> BasicTokenizerTester.testIsItOne(1));
-        testCases.put("IsItOne2 - demo failing test ",
-                () -> BasicTokenizerTester.testIsItOne(2));
-        testCases.put("ThrowAnException - demo test that throws exception ",
-                () -> BasicTokenizerTester.testThrowAnException());
+        //Test CountTokens
+        testCases.add(new TestCountTokens("TC007 -- CountTokens normal.", new BasicTokenizer("a b c d"), 4));
+        testCases.add(new TestCountTokens("TC007 -- CountTokens no delimiter.", new BasicTokenizer("abcd"), 1));
+        errCases.add(new TestCountTokens("TC007 -- CountTokens null delimiter. Throws exception.", new BasicTokenizer("a b c d", null), 0));
+        //etc....
 
-        //Execute test cases
-        BasicTokenizerTester.executeTestCases(testCases);
+        //Test HasMoreTokens
+        
+        //Test NextToken
+        
+        //Test NextToken(delim)
+        
+        //Test HasMoreElements
+        
+        //Test NextElement
+        
+        //Ad Hoc test will need a new TestCase for each scenario
+        
+        
+        executeTestCases(testCases, errCases);
+        
+        
     } // end main
 
 } // end class BasicTokenizerTester
