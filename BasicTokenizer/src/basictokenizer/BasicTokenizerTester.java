3e2b946e44b72c139c58bbf33510acc13345a409
Jamie White <robertjamisonwhite@gmail.com>
2015-09-30 22:42:15 -0500

    removed unused imports

--- BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
+++ BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
@@ -10,9 +10,6 @@ package basictokenizer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.concurrent.Callable;
 
 /**
  * <p>
