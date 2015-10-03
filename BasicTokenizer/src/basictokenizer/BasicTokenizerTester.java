a5350f60874891dcebc73e0a182bfff4b01f032d
Salina <Salina@Salina-PC>
2015-10-02 22:10:53 -0500

    added a test case t058 and updated some test case numbers

--- BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
+++ BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
@@ -136,22 +136,67 @@ public class BasicTokenizerTester {
         //etc....
 
         //Test HasMoreTokens
-        evaluateTestCase("TXXX", "hasMoreTokens normal.",
+        evaluateTestCaseException("T040", "R2 hasMoreTokens empty string, null delim.",
+                NullPointerException.class,
+                () -> (new BasicTokenizer("", null)).hasMoreElements());
+
+        evaluateTestCase("T041", "R3 hasMoreTokens empty string, empty delim.",
+                false,
+                () -> (new BasicTokenizer("", "")).hasMoreElements());
+
+        evaluateTestCase("T042", "R4 hasMoreTokens empty string, valid delim.",
+                false,
+                () -> (new BasicTokenizer("", ";")).hasMoreElements());
+
+        evaluateTestCaseException("T043", "R5 hasMoreTokens non-delimited string, null delim.",
+                NullPointerException.class,
+                () -> (new BasicTokenizer("abcd", null)).hasMoreElements());
+
+        evaluateTestCase("T044", "R6 hasMoreTokens non-delimited string, empty delim.",
+                true,
+                () -> (new BasicTokenizer("abcd", "")).hasMoreElements());
+
+        evaluateTestCase("T045", "R7 hasMoreTokens non-delimited string, valid delim, delims are not tokens.",
+                true,
+                () -> (new BasicTokenizer("abcd", ";", false)).hasMoreElements());
+
+        evaluateTestCase("T046", "R8 hasMoreTokens non-delimited string, valid delim, delims are tokens.",
+                true,
+                () -> (new BasicTokenizer("abcd", ";", true)).hasMoreElements());
+
+        evaluateTestCaseException("T047", "R9 hasMoreTokens delimited string, null delim.",
+                NullPointerException.class,
+                () -> (new BasicTokenizer("a b c d", null)).hasMoreElements());
+
+        evaluateTestCase("T048", "R10 hasMoreTokens delimited string, empty delim.",
+                true,
+                () -> (new BasicTokenizer("a b c d", " ")).hasMoreElements());
+
+        evaluateTestCase("T049", "R11 hasMoreTokens delimited string, valid delim, delims are not tokens.",
+                true,
+                () -> (new BasicTokenizer("a b,c d", " ", false)).hasMoreElements());
+
+        evaluateTestCase("T050", "R12 hasMoreTokens delimited string, valid delim, delims are tokens.",
+                true,
+                () -> (new BasicTokenizer("a b,c d", " ", true)).hasMoreElements());
+
+        evaluateTestCase("T051", "hasMoreTokens normal.",
                 true,
                 () -> (new BasicTokenizer("a b c d")).hasMoreTokens());
 
-        evaluateTestCase("TXXX", "hasMoreTokens no delims in text.",
+        evaluateTestCase("T052", "hasMoreTokens no delims in text.",
                 true,
                 () -> (new BasicTokenizer("abcd")).hasMoreTokens());
 
-        evaluateTestCase("TXXX", "hasMoreTokens empty text.",
+        evaluateTestCase("T053", "hasMoreTokens empty text.",
                 true,
                 () -> (new BasicTokenizer("")).hasMoreTokens());
 
-        evaluateTestCase("TXXX", "hasMoreTokens null delim. Should fail.",
+        evaluateTestCase("T053", "hasMoreTokens null delim. Should fail.",
                 false,
                 () -> (new BasicTokenizer("a b c d", null)).hasMoreTokens());
-
+        
+        
         //Test NextToken
         evaluateTestCase("T055", "nextToken normal.",
                 "a",
@@ -170,6 +215,7 @@ public class BasicTokenizerTester {
                 () -> (new BasicTokenizer("")).nextToken());
         
         //Test HasMoreElements
+        
         evaluateTestCase("T101", "hasMoreElements normal",
                 true,
                 () -> (new BasicTokenizer("a b c d")).hasMoreElements());
