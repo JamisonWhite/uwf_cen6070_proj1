b004a7755e6ae4df3fdffb6427e8333b744b7444
Salina <Salina@Salina-PC>
2015-10-02 21:06:13 -0500

    added a test case t058 and updated some test case numbers

--- BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
+++ BasicTokenizer/src/basictokenizer/BasicTokenizerTester.java
@@ -93,7 +93,7 @@ public class BasicTokenizerTester {
                 () -> (new BasicTokenizer("a b c d", "", true)) != null);
         
         evaluateTestCase("T014", "Constructor 3 inside boundary string, one character delimiter, return delimiter.",
-                true,
+                true,.
                 () -> (new BasicTokenizer("a b c d", "b", true)) != null);
         
         evaluateTestCase("T015", "Constructor 3 inside boundary string, inside boundary delimiter, return delimiter.",
@@ -165,8 +165,8 @@ public class BasicTokenizerTester {
                 NullPointerException.class,
                 () -> (new BasicTokenizer("a b c d", null)).nextToken());
         
-        evaluateTestCaseException("T058", "nextToken  empty text.",
-                NullPointerException.class,
+        evaluateTestCaseException("T058", "nextToken empty text.",
+                NoSuchElementException.class,
                 () -> (new BasicTokenizer("")).nextToken());
         
         //Test HasMoreElements
